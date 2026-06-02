5591118df3d5b3a396e5a4e4d271ec7750721d22
CarneiroSilveira <ian.silveira@alunos.sc.senac.br>
2026-06-02 18:00:19 -0300

    init: primeiro commit sucedido!

---
+++ base.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import utils.views_utils as util
+
+# Configurações para deixar a exibição mais legível/ Sim Professora eu copiei essa parte dos exercicios. Mas só desta parte😅
+pd.set_option('display.max_columns', None)   # Mostrar todas as colunas
+pd.set_option('display.float_format', '{:.2f}'.format)  # 2 casas decimais
+
+# importando as bibliotecas necessarias pro projeto 
+# O argumento sep=";" serve para que a separação das colunas do arquivo sejam defididas por ; que é o padrão dos arquivos .csv
+
+df = pd.read_csv("./Base-Varejo/Base-Varejo.csv",sep=";")
+
+# 1.
+util.views_brutecsv_statistics(df)
---
+++ utils/views_utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# o argumento df: pd.DataFrame é para que a função só consiga receber Data Fremes.
+
+def views_brutecsv_statistics(df: pd.DataFrame, nome = "DataFrame"):
+#   Trecho de codigo usado dos exemplos da aula teorica
+    print("=" * 55)
+    print(f"  📊 DIAGNÓSTICO: {nome}")
+    print("=" * 55)
+    print(f"  Linhas:           {df.shape[0]:,}")
+    print(f"  Colunas:          {df.shape[1]}")
+    print(f"  Linhas duplicadas:{df.duplicated().sum():,}")
+    print()
+    
+    nulos = df.isnull().sum()
+    pct   = (nulos / len(df) * 100).round(1)
+    
+    print("  Coluna           | Tipo       | Nulos | % Nulos")
+    print("  " + "-"*50)
+    for col in df.columns:
+        tipo = str(df[col].dtype)
+        print(f"  {col:<18}| {tipo:<10} | {nulos[col]:<5} | {pct[col]}%")
+    print("=" * 55)
